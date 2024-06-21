54df4341f03983aaec88bc1faab3f6a8da1ce7be
afraa786 <164616884+afraa786@users.noreply.github.com>
2024-06-21 13:44:07 +0530

    Add files via upload

---
+++ rock_paper_sc/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.Random;
+public class Main {
+    public static void main(String[] args) {
+        int rock = 0;
+        int paper = 1;
+        int scissor = 2;
+        Scanner fareed = new Scanner(System.in);
+        System.out.println("enter 0 for ROCK\n enter 1 for paper\n enter 2 for scissor\n ");
+        Scanner scan = new Scanner(System.in);
+        int fareedplays = scan.nextInt();
+        Random rc = new Random();
+        int computerplays = rc.nextInt(3);
+        if (computerplays==fareedplays){
+            System.out.println("Draw! Play again");
+        }
+        else if (computerplays==0 && fareedplays==1 || computerplays==2 && fareedplays==0 || computerplays==1 && fareedplays==2) {
+            System.out.println("YOU WIN");
+        }
+        else {
+            System.out.println("you lose, try again");
+        }
+        System.out.println("The computer choosed: " + computerplays);
+        if (computerplays==0) {
+            System.out.println("The computer choosed rock");
+        }
+        else if (computerplays==1){
+            System.out.println("The computer choosed paper");
+        }
+        else {
+            System.out.println("The computer choosed scissor");
+        }
+    }
+}
