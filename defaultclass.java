43e7232929e2b2f93aaa7b0421874d6d3a2166f7
Afraa Shaikh <164616884+afraa786@users.noreply.github.com>
2024-07-10 21:40:55 +0530

    Add files via upload
    This code implements a smartphone class with camera and wifi functionalities, and the main method demonstrates retrieving and printing available networks.

---
+++ defaultclass.java
@@ -0,0 +1,42 @@
+interface Mycamera {
+    void takesnap();
+    void recordvideo();
+}
+interface mywifi {
+    String [] getnetworks();
+    void connectToNetwork (String network);
+}
+class Mysmartphone {
+     void callNumber(int phonenumber) {
+         System.out.println("calling" + phonenumber);
+     }
+     void pickcall () {
+         System.out.println("Connecting...");
+     }
+}
+class Mycellphone extends Mysmartphone implements mywifi, Mycamera {
+public void takesnap() {
+    System.out.println("Taking Snap");
+}
+public void recordvideo() {
+    System.out.println("Take a Snap");
+}
+public String [] getnetworks() {
+    System.out.println("Getting list of networks");
+    String [] networks = {"FAREED,AFRAA, PRATIMA"};
+    return networks;
+}
+    @Override
+    public void connectToNetwork(String networks) {
+        System.out.println("Connecting to" + networks);
+    }
+}
+public class defaultclass {
+    public static void main (String[] args) {
+        Mycellphone IOS = new Mycellphone ();
+        String[] arr = IOS.getnetworks();
+        for (String network : arr) {
+            System.out.println(network);
+        }
+    }
+}
