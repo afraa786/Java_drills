c1a9bba9442169192cbd45bb268ef097f0d22b88
Afraa Shaikh <164616884+afraa786@users.noreply.github.com>
2024-08-18 00:36:45 +0530

    Add files via upload

---
+++ ZipFile.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+public class ZipFile {
+    public static void main(String[] args) {
+        String sourceFile = "website.txt";
+        String zipFileName = "fareed.zip";
+        try (
+                FileInputStream fis = new FileInputStream(sourceFile);
+                FileOutputStream fos = new FileOutputStream(zipFileName);
+                ZipOutputStream zipOut = new ZipOutputStream(fos)
+        ) {
+            File fileToZip = new File(sourceFile);
+            ZipEntry zipEntry = new ZipEntry(fileToZip.getName());
+            zipOut.putNextEntry(zipEntry);
+            byte[] bytes = new byte[1024];
+            int length;
+            while ((length = fis.read(bytes)) >= 0) {
+                zipOut.write(bytes, 0, length);
+            }
+            zipOut.closeEntry();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
