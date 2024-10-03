139169115df704270aa5e7fcdd7be9b211b06d3e
Afraa Shaikh <164616884+afraa786@users.noreply.github.com>
2024-10-03 10:37:59 +0530

    JAVA SWING GAME

---
+++ SnakeGame.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class SnakeGame {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        GamePanel gamePanel = new GamePanel();
+        frame.add(gamePanel);
+        frame.setTitle("Snake Game");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+    }
+}
