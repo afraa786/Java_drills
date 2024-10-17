60d81433c7a08922a6109a8d693a9472817676ae
afraa786 <164616884+afraa786@users.noreply.github.com>
2024-10-17 23:01:40 +0530

    basic networking project

---
+++ comparision/Largest.java
@@ -0,0 +1,8 @@
+package comparision;
+// File: Largest.java
+public class Largest {
+    // Method to find the largest of two numbers
+    public int findLargest(int num1, int num2) {
+        return (num1 > num2) ? num1 : num2;
+    }
+}
